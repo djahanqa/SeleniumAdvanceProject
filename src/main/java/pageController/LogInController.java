30d9292a9385a1d0ebe70895e7a7cf75804e52cd
dilrubajahan <djahanqa@gmail.com>
2020-05-04 22:11:49 -0400

    created signin button

--- src/main/java/pageController/LogInController.java
+++ src/main/java/pageController/LogInController.java
@@ -1,4 +1,21 @@
 package pageController;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LogInController {
+
+    @FindBy(xpath ="//*[@id='header']//a[contains(text(),'Sign in')]")
+    WebElement signInButton;
+
+    public LogInController(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+
+        public void signInTab(){
+
+
+    }
 }
--- src/test/java/logIn/LogInTest.java
+++ src/test/java/logIn/LogInTest.java
@@ -1,4 +1,24 @@
 package logIn;
 
-public class LogInTest {
+import base.ScriptBase;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pageController.LogInController;
+
+public class LogInTest extends ScriptBase {
+
+     LogInController login;
+     @BeforeTest
+     public void init(){
+         BeforeTest();
+
+     }
+
+
+    @Test
+    public void vrtifySignInButton(){
+        login =new LogInController(driver);
+        login.signInTab();
+
+    }
 }
